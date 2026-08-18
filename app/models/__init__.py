15382c6b700cd4d6c8c0db17cbe20d4429583289
RGDeveloper-UCT <hgarciamonitoreouct@gmail.com>
2026-08-18 12:04:44 -0600

    feat: registrar modelo de importaciones de portadores

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.bitacora import Bitacora
 from app.models.documento_expediente import DocumentoExpediente
 from app.models.alerta import Alerta
 from app.models.prestamo import PrestamoExpediente
+from app.models.importacion_portadores import ImportacionPortadores
 from app.models.coordinacion import (
     RegistroCoordinacion,
     PagoCoordinacion,
