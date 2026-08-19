21dc480a8028a57deb36eb1697b075b21e7c0379
RGDeveloper-UCT <hgarciamonitoreouct@gmail.com>
2026-08-19 08:57:05 -0600

    Registrar ruta admin de eliminación de SP

--- app/__init__.py
+++ app/__init__.py
@@ -57,12 +57,12 @@ def create_app():
         return usuario
 
     from app.routes import (
-        auth_bp, dashboard_bp, expedientes_bp, expediente_fisico_bp, verificaciones_bp,
+        auth_bp, dashboard_bp, expedientes_bp, expedientes_admin_bp, expediente_fisico_bp, verificaciones_bp,
         bitacora_bp, indice_documental_bp, alertas_bp, prestamos_bp, admin_bp,
         integridad_bp, busqueda_bp, cuenta_bp, coordinacion_bp, portadores_bp,
     )
     for blueprint in (
-        auth_bp, dashboard_bp, expedientes_bp, expediente_fisico_bp, verificaciones_bp,
+        auth_bp, dashboard_bp, expedientes_bp, expedientes_admin_bp, expediente_fisico_bp, verificaciones_bp,
         bitacora_bp, indice_documental_bp, alertas_bp, prestamos_bp, admin_bp,
         integridad_bp, busqueda_bp, cuenta_bp, coordinacion_bp, portadores_bp,
     ):
