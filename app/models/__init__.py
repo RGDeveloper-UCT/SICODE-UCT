5bb7e4a4f6e9ebca2a7c01d32047b8f255a50028
RGDeveloper-UCT <hgarciamonitoreouct@gmail.com>
2026-08-19 09:53:11 -0600

    Models: registrar traslados virtuales

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -5,6 +5,7 @@ from app.models.bitacora import Bitacora
 from app.models.documento_expediente import DocumentoExpediente
 from app.models.alerta import Alerta
 from app.models.prestamo import PrestamoExpediente
+from app.models.traslado_virtual import TrasladoVirtualExpediente
 from app.models.importacion_portadores import ImportacionPortadores
 from app.models.verificacion import VerificacionExpediente
 from app.models.coordinacion import (
