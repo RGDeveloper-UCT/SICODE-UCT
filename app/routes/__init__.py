bfbd456c614de6acc7ec496ec74d3991cb31973d
REB <hgarciamonitoreouct@gmail.com>
2026-09-01 08:54:22 -0600

    Registrar blueprint del modulo de pagos

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -13,6 +13,7 @@ from app.routes.admin import admin_bp
 from app.routes.integridad import integridad_bp
 from app.routes.busqueda import busqueda_bp
 from app.routes.cuenta import cuenta_bp
+from app.routes.pagos import pagos_bp
 from app.routes.coordinacion import coordinacion_bp
 from app.routes import coordinacion_expediente_completo as _coordinacion_expediente_completo
 from app.routes.coordinacion_export import coordinacion_export_bp
